9eb2da424d8157b5903ad6daa7bc6f7cc7bd75f4
Shahadat Zaman Usan <shahadatzamanusan@gmail.com>
2022-08-24 01:21:29 +0600

    example

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Welcome to our top Bike listing!\n")
+
+input("Please press enter to see the list!\n")
+Bikes=["YAMAHA","SUZUKI","HONDA","ROYAL INFIELD","KAWASAKI"]
+
+for bike in Bikes:
+  print(bike)
+  print(bike + "$5000")
+print( f"These bikes are from the list is given below\n {Bikes}")
+
